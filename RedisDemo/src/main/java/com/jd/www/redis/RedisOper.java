585b4e5bea2bbba5fd228f9c1c338df594385127
qiuxiangu <qiuxiangu@jd.com>
2016-06-24 17:02:17 +0800

    20160623

--- RedisDemo/src/main/java/com/jd/www/redis/RedisOper.java
+++ RedisDemo/src/main/java/com/jd/www/redis/RedisOper.java
@@ -1,6 +1,10 @@
 package com.jd.www.redis;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import redis.clients.jedis.Jedis;
+
+import java.util.List;
 
 import static com.jd.www.redis.RedisUtil.getJedis;
 import static com.jd.www.redis.RedisUtil.returnResource;
@@ -9,6 +13,7 @@ import static com.jd.www.redis.RedisUtil.returnResource;
  * Created by qiuxiangu on 2016/6/23.
  */
 public class RedisOper {
+    private static final Logger LOG = LoggerFactory.getLogger(RedisOper.class);
     private Jedis jedis;
     public RedisOper() {
         jedis = getJedis();
@@ -34,6 +39,11 @@ public class RedisOper {
         return value;
     }
 
+    /**
+     * value值追加
+     * @param key
+     * @param value
+     */
     public void append(String key, String value) {
         if (jedis.exists(key)) {
             jedis.append(key, value);
@@ -43,4 +53,36 @@ public class RedisOper {
         returnResource(jedis);
     }
 
+    public List<String> blpop(String[] key) {
+        try {
+            LOG.info("blpop");
+           return jedis.blpop(1,key);
+        } finally {
+            returnResource(jedis);
+        }
+    }
+
+    public void lpush(String key, String value) {
+        try {
+            jedis.lpush(key, value);
+        } finally {
+            returnResource(jedis);
+        }
+    }
+
+    public void rpush(String key, String value) {
+        try {
+            jedis.rpush(key, value);
+        } finally {
+            returnResource(jedis);
+        }
+    }
+
+    public void rpop(String key) {
+        try {
+            jedis.rpop(key);
+        } finally {
+            returnResource(jedis);
+        }
+    }
 }
--- RedisDemo/src/test/java/com/jd/www/redis/RedisOperTest.java
+++ RedisDemo/src/test/java/com/jd/www/redis/RedisOperTest.java
@@ -2,10 +2,42 @@ package com.jd.www.redis;
 
 import org.junit.Test;
 
+import java.util.List;
+
 /**
  * Created by qiuxiangu on 2016/6/23.
  */
 public class RedisOperTest {
+    @Test
+    public void rpush() throws Exception {
+
+    }
+
+    @Test
+    public void rpop() throws Exception {
+
+    }
+
+    RedisOper oper = new RedisOper();
+    @Test
+    public void set() throws Exception {
+
+    }
+
+    @Test
+    public void blpop() throws Exception {
+        String[] keys =new String[] {"mylist", "mylist1"};
+        List<String> list = oper.blpop(keys);
+        for(String str : list) {
+            System.out.println(str);
+        }
+    }
+
+    @Test
+    public void lpush() throws Exception {
+        oper.lpush("mylist", "123");
+    }
+
     @Test
     public void append() throws Exception {
         RedisOper oper = new RedisOper();
