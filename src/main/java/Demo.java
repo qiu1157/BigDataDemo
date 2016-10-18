d6ad931ffb5f8a2b30e5ec370d0da2763733470d
qiuxiangu <qiuxiangu@jd.com>
2016-10-18 10:59:09 +0800

    20160929

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -1,17 +1,8 @@
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
-
 /**
  * Created by qiuxiangu on 2016/6/6.
  */
 public class Demo {
     public static void main(String[] args) {
-        String str = "a,b,c";
-        String[] s = str.split(",", -1);
-        Set<String> set = new HashSet(Arrays.asList(s));
-        for(String tmp : set) {
-            System.out.println(tmp);
-        }
+
     }
 }
