b72a4e581dba459b383191d5f4651e184b52c9fa
qiuxiangu <qiuxiangu@jd.com>
2016-10-25 01:53:31 +0800

    1

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -1,8 +1,20 @@
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
 /**
  * Created by qiuxiangu on 2016/6/6.
  */
 public class Demo {
     public static void main(String[] args) {
-
+        while (true) {
+            DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            long time = System.currentTimeMillis();
+            System.out.println(formatter.format(time));
+            try {
+                Thread.sleep(60000l);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
